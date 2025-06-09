4369ecafaa22e67eeef074175dc80eb7704314d9
Shah <minthassan7628@gmail.com>
2025-06-09 21:49:49 +0500

    Adding .gitignore

---
+++ closure.py
@@ -0,0 +1,21 @@
+def closer(num):
+    def actual(x):
+        return num**x
+
+    return actual
+
+
+a = closer(4)
+print(a(4))
+
+
+# Lets understand how closer work:
+
+# You make a function called power that takes an argument num.
+def power(num):
+    def value(val):
+        return val**num  # Inside you create another function called value that takes another argument val and it does val to the power of num
+    
+    # so when you call power(num) it gives you a new function value that remembers num
+    return value
+        
